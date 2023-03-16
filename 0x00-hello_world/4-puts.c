884dec41410394bc5af6a2f0e0082539c0f33647
KuriaTimo <kuriatimm@gmail.com>
2023-03-16 13:53:44 -0700

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *
+  *Puts - prints out a string
+  *
+  *Return: 0 a success
+  */
+
+int main(void)
+{
+	char prog[] = "\"Programming is like building a multilingual puzzle";
+
+	puts(prog);
+
+	return (0);
+}
