3db0327f02f49ca4c9434606f093147f3e7ab794
KuriaTimo <kuriatimm@gmail.com>
2023-03-21 15:46:51 -0700

    Absolute number

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+  * _abs - prints absolute value
+  *
+  * Definitions:
+  *
+  *@n: is an integer
+  *
+  * Return: absolute integer
+  */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else if (n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
+}
