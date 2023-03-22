d5ad1efe653fbbc093638ef020070b05d3a8e5b4
KuriaTimo <kuriatimm@gmail.com>
2023-03-21 18:44:45 -0700

    Times table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,53 @@
+#include "main.h"
+
+/**
+  * print_times_table - prints the times table
+  * Description: Prints the times table
+  * @n: integer
+  * Return: void
+  */
+
+void print_times_table(int n)
+{
+	int row, column, product;
+
+	n = 0;
+	if (n >= 0 && n < 15)
+	{
+		row = 0;
+		while (row <= n)
+		{
+			column = 0;
+			while (column <= n)
+			{
+				product = row * column;
+				if (column == 0)
+					_putchar('0');
+				else if (product < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(product % 10 + '0');
+				}
+				else if (product >= 10 && product < 100)
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				else if
+					_putchar(',');
+					_putchar(' ');
+					_putchar(product / 100 + '0');
+					_putchar((product / 10) % 10 + '0');
+					_putchar(product % 10 + '0');
+				column++;
+			}
+			_putchar('\n');
+			row++;
+		}
+	}
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -25,4 +25,5 @@ int add(int, int);
 
 void print_to_98(int n);
 
+void ptint_times_tables(int n);
 #endif
