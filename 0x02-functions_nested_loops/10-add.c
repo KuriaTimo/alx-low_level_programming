99fb75bc6b06eef2f35a3bba58c6f0df563e12b8
KuriaTimo <kuriatimm@gmail.com>
2023-03-21 17:59:23 -0700

    function to add integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+  * add - adds integers
+  *
+  * Description: adds two integers
+  *
+  * @x: integer
+  * @y: integer
+  * Return: result
+  */
+
+int add(int x, int y)
+{
+	int result;
+
+	result = x + y;
+
+	return (result);
+}
