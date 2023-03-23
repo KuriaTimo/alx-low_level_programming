49bc88a8e7220b3243ec5fd572191c5256d6a0b8
KuriaTimo <kuriatimm@gmail.com>
2023-03-23 16:09:03 -0700

    Func prints most numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+  * print_most_numbers - prints numbers 0-9 except 2,4
+  *
+  * Return: Void
+  */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	c = 0;
+
+	while (c < 10)
+	{
+		if(c != 2 && c != 4)
+			_putchar(c);
+		c++;
+	}
+	_putchar ('\n');
+}
+
