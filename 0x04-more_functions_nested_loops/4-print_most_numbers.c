ce5652e0a1a00bd9c8303770553eca8567ea99c9
KuriaTimo <kuriatimm@gmail.com>
2023-03-23 16:14:02 -0700

    Func to print most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,12 +10,12 @@ void print_most_numbers(void)
 {
 	int c;
 
-	c = 0;
+	c = '0';
 
-	while (c < 10)
+	while (c <= '9')
 	{
-		if(c != 2 && c != 4)
-			_putchar(c);
+		if(c != '2' && c != '4')
+		_putchar(c);
 		c++;
 	}
 	_putchar ('\n');
