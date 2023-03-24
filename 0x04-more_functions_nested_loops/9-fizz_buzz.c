6f2f686e85f813c5704d8567378a1c88083c567b
KuriaTimo <kuriatimm@gmail.com>
2023-03-23 18:06:11 -0700

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+  * main - FizzBuzz test
+  *
+  * Descriptions: Prints 1-100
+  *
+  * Return: 0
+  */
+
+int main(void)
+{
+	int i;
+
+	i = 1;
+
+	while (i <= 100)
+	{
+		if ((i % 3 == 0) && (i % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
