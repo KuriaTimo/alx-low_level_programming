0ed2cc90d0fa47a50f4a75b7de1984962b49cc42
KuriaTimo <kuriatimm@gmail.com>
2023-03-27 17:19:17 -0700

    Pointer

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+  * main: reset_to_98 - function that set an new value
+  * @n: pointer
+  *
+  *Return: Void
+  */
+
+void reset_to_98(int *n)
+{
+	int num = 98;
+	*n = num;
+}
